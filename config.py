b99707efbc52b28c05e6ef677f8c98378a84f568
Arsen <94738158+ArsenGad007@users.noreply.github.com>
2024-03-30 11:35:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,10 @@ MAX_TOKENS_IN_TASK = 300
 MAX_TOKENS_IN_ANSWER = 100
 ENDPOINT = 'http://localhost:1234/v1/chat/completions'
 HEADER = {"Content-Type": "application/json"}
-IAM_TOKEN = ("t1.9euelZqLx8-Xy8yLlZaYl8rOlsqRlO3rnpWanpOOjZydmIqUy5GJk5aJj8vl8_cUPGNP"
-             "-e8OWDYU_t3z91RqYE_57w5YNhT"
-             "-zef1656VmomPk8mLicyXycjKksuVipyT7_zF656VmomPk8mLicyXycjKksuVipyTveuelZrOi5uUkMyKyZ2TxprGxs3NmrXe"
-             "hpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.N6eTE29IIw0TYItkkvkJuObN-INJxR1hsU4TwMVWbFi0XkIACJX6"
-             "KTZXlI0kJ7DMuIuoV51_DOm9w7XKo0QvBA")
+IAM_TOKEN = ("t1.9euelZqWlZWWx5mLl42NlZKZmJCSmO3rnpWanpOOjZydmIqUy5GJk5aJj8vl8_deamBP-e8oVTRH_d3z9x4ZXk_57yhVNEf9zef165"
+             "6VmpOPk8qQjpiYzI7Jx5eciYvK7_zF656VmpOPk8qQjpiYzI7Jx5eciYvKveuelZrHnJuQy52Vmo2QzZjNnMiYlLXehpzRnJCSj4qLmtG"
+             "LmdKckJKPioua0pKai56bnoue0oye.hAV9CFo4du464x2l7rShDFk7Y-rN43LDyNrD7l9bqd_XC_8tSj3QxfCvn1gFnhHOVAsECUlzHS_"
+             "uWKIaWE0NDQ")
 FOLDER_ID = "b1gamvjok1q4hag6gjmj"
 COUNT_SESSION = 3
 TOKENS_IN_SESSION = 400
